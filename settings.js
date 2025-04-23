068789ac2d0b7b54c23806f22d9b931055259d24
Sahasaalim <160024911+Sahasaalim@users.noreply.github.com>
2025-04-23 19:03:52 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America/Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
-global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
+global.ownernumber = '233203021598'  //creator number
+global.ownername = 'Anonymous369 //owner name
 global.botname = 'DRK_ST_BUG' //name of the bot
 
 //sticker details
